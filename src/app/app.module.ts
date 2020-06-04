1ad93ed913fd314ea2be49423fa1561d88695c0f
Cesar kennedy rousseau arzapalo caldas <cesar.arzapalo@unmsm.edu.pe>
2020-06-03 23:20:20 -0500

    subiendo modificaciones de componentes 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TodoModule } from './todos/todo.module';
+import { FooterComponent } from './footer/footer.component';
 
-import { TodoAddComponent } from './todos/todo-add/todo-add.component';
-import { TodoListaComponent } from './todos/todo-lista/todo-lista.component';
-import { TodoItemComponent } from './todos/todo-item/todo-item.component';
-import { TodoPageComponent } from './todos/todo-page/todo-page.component';
-import { TodoFooterComponent } from './todos/todo-footer/todo-footer.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoAddComponent,
-    TodoListaComponent,
-    TodoItemComponent,
-    TodoPageComponent,
-    TodoFooterComponent
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- src/app/todos/todo.module.ts
+++ src/app/todos/todo.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { TodoAddComponent } from './todo-add/todo-add.component';
+import { TodoListaComponent } from './todo-lista/todo-lista.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
+import { TodoPageComponent } from './todo-page/todo-page.component';
+import { TodoFooterComponent } from './todo-footer/todo-footer.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TodoAddComponent,
+    TodoListaComponent,
+    TodoItemComponent,
+    TodoPageComponent,
+    TodoFooterComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    TodoPageComponent
   ]
 })
 export class TodoModule { }
