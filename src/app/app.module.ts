024319d23e7da006786e09c430fcdc9d2e906eaa
Cesar kennedy rousseau arzapalo caldas <cesar.arzapalo@unmsm.edu.pe>
2020-06-03 23:11:36 -0500

    subiendo modificaciones de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TodoModule } from './todos/todo.module';
+
+import { TodoAddComponent } from './todos/todo-add/todo-add.component';
+import { TodoListaComponent } from './todos/todo-lista/todo-lista.component';
+import { TodoItemComponent } from './todos/todo-item/todo-item.component';
+import { TodoPageComponent } from './todos/todo-page/todo-page.component';
+import { TodoFooterComponent } from './todos/todo-footer/todo-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoAddComponent,
+    TodoListaComponent,
+    TodoItemComponent,
+    TodoPageComponent,
+    TodoFooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TodoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/todos/todo-lista/todo-lista.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-lista',
+  templateUrl: './todo-lista.component.html',
+  styleUrls: ['./todo-lista.component.css']
+})
+export class TodoListaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
